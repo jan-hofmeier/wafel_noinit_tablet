2267c62175c52c15938746a3262ece0209db08fe
Jan Hofmeier <jan.hofmeier@gmail.com>
2023-08-12 22:55:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Stroopwafel plugin to keep the tablet off
+
+This plugin for [stroopwafel](https://github.com/shinyquagsire23/stroopwafel) a IOSU patch to not turn on the tablet / gamepad on boot.
+
+## How to use
+
+Put the `wafel_noinit_tablet.ipx` together with `wafel_core.ipx` from stroopwafel in /wiiu/ios_plugins on your sdcard
+
+## Building
+
+```bash
+export STROOPWAFEL_ROOT=/path/too/stroopwafel-repo
+make
+```
+
+## Thanks
+
+- [**GaryOderNichts**](https://github.com/GaryOderNichts) for the patch
---
+++ source/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <wafel/dynamic.h>
+#include <wafel/ios_dynamic.h>
+#include <wafel/utils.h>
+#include <wafel/patch.h>
+#include <wafel/ios/svc.h>
+#include <wafel/ios/memory.h>
+
+// This fn runs before everything else in kernel mode.
+// It should be used to do extremely early patches
+// (ie to BSP and kernel, which launches before MCP)
+// It jumps to the real IOS kernel entry on exit.
+__attribute__((target("arm")))
+void kern_main()
+{
+    // Make sure relocs worked fine and mappings are good
+    debug_printf("we in here noinit_tablet patch kern %p\n", kern_main);
+
+    debug_printf("init_linking symbol at: %08x\n", wafel_find_symbol("init_linking"));
+
+    // stub the WOWL command in the IOSU
+    ASM_PATCH_K(0x11f55b1c, "bx lr")
+
+    debug_printf("noinit_tablet patch applied\n");
+}
+
+// This fn runs before MCP's main thread, and can be used
+// to perform late patches and spawn threads under MCP.
+// It must return.
+void mcp_main()
+{
+
+
+}
